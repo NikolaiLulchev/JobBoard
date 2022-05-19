33f7a4576c02464a8b2ba78cb0949c3efdab7d43
NikolaiLulchev <nikolai.lulchev@gmail.com>
2022-05-19 17:01:54 +0300

    Added some base HTML

---
+++ src/main/java/org/softuni/jobboard/web/WebController.java
@@ -0,0 +1,43 @@
+package org.softuni.jobboard.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/offers")
+    public String offers() {
+        return "offers";
+    }
+
+    @GetMapping("/add-offer")
+    public String addOffer() {
+        return "add-offer";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "redirect:/";
+    }
+}
